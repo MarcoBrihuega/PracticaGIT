95f66f660480ace0d9ce4c47573224a838b8814b
MaroBrihuega <marcobrihuega@gmail.com>
2024-03-18 13:03:18 +0100

    Añado un metodo obsoleto para la clase InteligenciaArtificial

--- PaqueteDeClases/GestionIA.java
+++ PaqueteDeClases/GestionIA.java
@@ -1,4 +1,15 @@
 package PaqueteDeClases;
 
+/**
+ *ESTA CLASE SE UTILIZA PARA PODER GESTIONAR EL OBJETO IA CAMBIANDOLE EL ID A LA IA Y CAMBIANDOLE EL NOMBRE A LA IA
+ * @author MARCO BRIHUEGA
+ * @version 1.2
+ *
+ */
 public class GestionIA {
+
+
+
+
+
 }
